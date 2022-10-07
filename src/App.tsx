7f49590590dfca549fb1ce296b65137085a86547
Antonio Neto <acgomesn@gmail.com>
2022-10-07 10:16:27 -0300

    implementado router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 
-import { defaultTheme } from './styles/themes/default'
-import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
+import { Router } from './Router';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>asdfasdf</div>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
