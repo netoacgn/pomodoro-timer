07c80c5771f0701f8ece0aa2d6c855f1f1baa3cd
Antonio Neto <acgomesn@gmail.com>
2022-10-25 10:33:45 -0300

    instalacao lib zod para validacao de formularios

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,20 @@
 import { Play } from "phosphor-react";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
 import { FormContainer, HomeContainer, CountdownContainer, Separator, StartCountdownButton, TaskInput, MinutesAmountInput } from "./styles";
 
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutesAmount: zod.number()
+        .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
+        .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+});
+
 export function Home() {
-    const { register, handleSubmit, watch } = useForm();
+    const { register, handleSubmit, watch } = useForm({
+        resolver: zodResolver(newCycleFormValidationSchema)
+    });
 
     function handleCreateNewCycle(data: any) {
         console.log(data)
@@ -38,8 +49,8 @@ export function Home() {
                         placeholder="00"
                         step={5}
                         min={5}
-                        max={60}
-                        {...register('minutesAmount', { valueAsNumber: true})}
+                        // max={60}
+                        {...register('minutesAmount', { valueAsNumber: true })}
                     />
 
                     <span>minutos.</span>
@@ -53,7 +64,7 @@ export function Home() {
                     <span>0</span>
                 </CountdownContainer>
 
-                <StartCountdownButton disabled={!isSubmitDisable} type="submit">
+                <StartCountdownButton disabled={isSubmitDisable} type="submit">
                     <Play size={24} />
                     Começar
                 </StartCountdownButton>
